1c43036ed1efaf6b83e2cf525804550b27650538
esmaeel <esmaeel.ghasemi.org@gmail.com>
2025-04-29 02:12:11 +0330

    Add Transaction class which is invokable

---
+++ src/QueryBuilder/Clauses/Transaction/Transaction.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Saraf\QB\QueryBuilder\Clauses\Transaction;
+
+use React\Promise\PromiseInterface;
+use Saraf\QB\QueryBuilder\Exceptions\DBFactoryException;
+
+class Transaction extends TransactionBuilder
+{
+    /**
+     * @throws DBFactoryException
+     */
+    public function __invoke(
+        callable $body
+    ): \React\Promise\PromiseInterface
+    {
+        return $this->startTransaction()
+            ->then(function () use ($body) {
+
+                $bodyPromise = $body(new TransactionQuery($this));
+
+                if (!$bodyPromise instanceof PromiseInterface) {
+
+                    throw new DBFactoryException("Transaction body should return promise");
+                }
+
+                return $bodyPromise;
+
+            })->then(function () {
+
+                $this->commit();
+
+            })->catch(function (\Throwable $e) {
+
+                $this->rollback();
+                exit(0);
+
+            });
+    }
+}
