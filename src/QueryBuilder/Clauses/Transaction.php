78813287e7f2caa185191ab8739a9806f7c9c33a
esmaeel <esmaeel.ghasemi.org@gmail.com>
2025-05-04 11:16:07 +0330

    Add handling Rollback errors and promise

--- src/QueryBuilder/Clauses/Transaction.php
+++ src/QueryBuilder/Clauses/Transaction.php
@@ -7,6 +7,7 @@ use Saraf\QB\QueryBuilder\Core\DBWorker;
 use Saraf\QB\QueryBuilder\Exceptions\TransactionException;
 use Saraf\QB\QueryBuilder\Helpers\QueryResult\QueryResult;
 use Saraf\QB\QueryBuilder\Helpers\QueryResult\QueryResultCollection;
+use function React\Promise\reject;
 
 class Transaction
 {
@@ -31,15 +32,15 @@ class Transaction
     /**
      * @throws TransactionException
      */
-    public function compile(): void
+    public function compile(): \React\Promise\PromiseInterface
     {
         if (count($this->queries) === 0) {
             throw new TransactionException('There are no queries inside transaction.');
         }
 
-        $this->connection->query("START TRANSACTION")
+        return $this->connection->query("START TRANSACTION")
             ->then(function () {
-                $this->resolveQueries();
+                return $this->resolveQueries();
             })
             ->finally(function () {
                 $this->dbFactory->releaseConnection($this->connection);
@@ -58,32 +59,31 @@ class Transaction
         $callback = $queryItem['callback'];
         $name = $queryItem['name'];
 
-        $queryString = null;
-        $query->compile()->getQuery()
-            ->then(function ($result) use (&$queryString) {
-                $queryString = $result['query'];
-            });
+        return $query->compile()->getQuery()->then(function ($result) use ($query, $callback, $name) {
+            $queryString = $result['query'];
+            return $this->connection->query($queryString)
+                ->then(function ($result) use ($query, $callback, $name, $queryString) {
+                    if (!$result['result']) {
+                        $this->connection->query('ROLLBACK');
+                        return reject(throw new TransactionException('Transaction rolled back due to ' . $result['error']));
+                    }
 
-        return $this->connection->query($queryString)
-            ->then(function ($result) use ($query, $callback, $name) {
-                if (!$result['result']) {
-                    return $this->connection->query('ROLLBACK');
-                }
+                    $queryResult = new QueryResult(
+                        $result['result'],
+                        @$result['count'] ?? null,
+                        @$result['rows'] ?? [],
+                        @$result['affectedRows'] ?? null,
+                        @$result['insertId'] ?? null,
+                    );
 
-                $queryResult = new QueryResult(
-                    $result['result'],
-                    @$result['count'] ?? null,
-                    @$result['rows'] ?? [],
-                    @$result['affectedRows'] ?? null,
-                    @$result['insertId'] ?? null,
-                );
+                    if ($callback($queryResult, $this->queryResultCollection)) {
+                        $this->queryResultCollection->add($name, $queryResult);
+                        return $this->resolveQueries();
+                    }
 
-                if ($callback($queryResult, $this->queryResultCollection)) {
-                    $this->queryResultCollection->add($name, $queryResult);
-                    return $this->resolveQueries();
-                }
-
-                return $this->connection->query('ROLLBACK');
-            });
+                    $this->connection->query('ROLLBACK');
+                    return reject(throw new TransactionException("Transaction rolled back,callback for query {$queryString} doesn't return true"));
+                });
+        });
     }
 }
